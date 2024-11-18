5a7c54beb6ba261f5c6ea9583b6bfc436d17d04e
Nick <nbell@bellsolve.co.uk>
2024-11-18 18:38:27 +0000

    Added a Europe calendar

---
+++ src/QLNet/Time/Calendars/Europe.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QLNet
+{
+   class Europe: TARGET
+   {
+      //the european calendar is simply an inheritance of the TARGET calendar.
+   }
+}
